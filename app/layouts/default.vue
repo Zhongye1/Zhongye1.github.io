5be2c3047e28a98a7db8bfe489ae913208fa5d8f
zhongye <2760913192@qq.com>
2026-09-23 05:26:58 +0800

    vw fix

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,7 +7,7 @@ import SideBarRight from './components/SideBarRight.vue'
 <template>
   <div class="container flex min-h-screen flex-col">
     <div
-      class="mx-auto flex w-full max-w-1200px flex-1 flex-col gap-8 px-6 py-8 lg:flex-row lg:gap-10 lg:px-8"
+      class="mx-auto flex w-full max-w-80vw flex-1 flex-col gap-8 px-6 py-8 lg:flex-row lg:gap-10 lg:px-8"
     >
       <SideBarLeft class="lg:w-200px lg:shrink-0" />
       <main class="min-w-0 flex-1">
